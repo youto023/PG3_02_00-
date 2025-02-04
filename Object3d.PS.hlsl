db64d190e7488301b5de46eb64e386342a312da1
youto023 <yuuto9036@gmail.com>
2025-02-04 22:44:14 +0900

    完成

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -18,6 +18,8 @@ SamplerState gSampler : register(s0);
 
 PixelShaderOutput main(VertexShaderOutput input)
 {
+    float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
+    
     float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     float32_t3 reflectLight = reflect(normalize(gDirectionalLight.direction), normalize(input.normal));
     
@@ -26,16 +28,18 @@ PixelShaderOutput main(VertexShaderOutput input)
     
     if (gMaterial.enableLighting != 0)
     { // Lightingする場合
+        
+      
+        float RdotE = dot(reflectLight, toEye);
+        //float specularPow = pow(saturate(RdotE), gMaterial.shininess);
     
         float NdotL = dot(normalize(input.normal), normalize(-gDirectionalLight.direction));
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
         
-        float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
-        float32_t3 reflectLight = reflect(normalize(-gDirectionalLight.direction), normalize(input.normal));
+        float32_t3 halfVector = normalize(-gDirectionalLight.direction * toEye);
+        float NDotH = dot(normalize(input.normal), halfVector);
+        float specularPow = pow(saturate(NDotH), gMaterial.shininess);
     
-        float RdotE = dot(reflectLight, toEye);
-        float specularPow = pow(saturate(RdotE), gMaterial.shininess);
-        
         float32_t3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
         // 鏡面反射
         float32_t3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float32_t3(1.0f, 1.0f, 1.0f);
